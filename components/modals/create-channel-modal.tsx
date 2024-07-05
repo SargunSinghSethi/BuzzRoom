e304c26bc8099de987882b1f1ff43b736300f9f4
Sargun Singh Sethi <100695155+SargunSinghSethi@users.noreply.github.com>
2024-07-05 20:02:47 +0530

    Server Search, Render Channels and Members in the Sidebar

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -34,7 +34,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import ServerIdLayout from "@/app/(main)/(routes)/servers/[serverId]/layout";
+import { useEffect } from "react";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -48,19 +48,29 @@ const formSchema = z.object({
 });
 
 export const CreateChannelModal = () => {
-  const { isOpen, onClose, type } = useModal();
+  const { isOpen, onClose, type, data } = useModal();
   const router = useRouter();
   const params = useParams();
 
   const isModalOpen = isOpen && type === "createChannel";
+  const {channelType } = data;
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      type: ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     },
   });
+
+
+  useEffect(() => {
+    if(channelType) {
+      form.setValue("type",channelType);
+    } else {
+      form.setValue("type",ChannelType.TEXT);
+    }
+  },[channelType, form]);
 
   const isLoading = form.formState.isSubmitting;
 
